7cf0e3c650a35cb5052a4fba4da218db13b467a7
YosefWel <164270640+YosefWel@users.noreply.github.com>
2024-07-23 17:51:57 +0200

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import "./header.css";
+import NetflixLogo from "../../assets/images/NetflixLogo.png";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+const Header = () => {
+  const [show, handleShow] = useState(false);
+
+  const scrollHandler = () => {
+    if (window.scrollY > 180) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollHandler);
+    return () => {
+      window.removeEventListener("scroll", scrollHandler);
+    };
+  }, []);
+  return (
+    <div className={`header_outer_container ${show && "nav__black"}`}>
+      <div className="header_container">
+        <div className="header_left">
+          <ul>
+            <li>
+              <img src={NetflixLogo} alt="Netflix Logo" width="100" />
+            </li>
+            <li>Home</li>
+            <li>TVShows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>MyList</li>
+            <li>Browse by Languages</li>
+          </ul>
+        </div>
+        <div className="header_right">
+          <ul>
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsNoneIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              <ArrowDropDownIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
